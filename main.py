4c0b8e688becb8a0518550e6e8c15e18c2104ba8
dg2026719-oss <dg2026719@danggok.hs.kr>
2026-05-19 09:11:12 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+cylinder(pos = vec(0,0,0), opacity = 1, size = vec(10,25,29), color = vector(0.82, 0.68, 0.45))
+cylinder(pos = vec(0,15.7,0), opacity = 1, size = vec(10,16,21), color = vector(0.82, 0.68, 0.45))
+cylinder(pos = vec(-0.1,11,0), opacity = 1, size = vec(8.3,8.3,8.3), color = color.black)
+box(pos = vec(0.5,31.7,0), opacity = 1, size = vec(2,34,5), color = vector(0.17,0.15,0.14))
