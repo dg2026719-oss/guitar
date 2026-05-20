8837555e60f51b2d58fcc15a4fc03f60b9e41123
dg2026719-oss <dg2026719@danggok.hs.kr>
2026-05-20 15:02:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+Web VPython 3.2
+
 cylinder(pos = vec(0,0,0), opacity = 1, size = vec(10,25,29), color = vector(0.82, 0.68, 0.45))
 cylinder(pos = vec(0,15.7,0), opacity = 1, size = vec(10,16,21), color = vector(0.82, 0.68, 0.45))
 cylinder(pos = vec(-0.1,11,0), opacity = 1, size = vec(8.3,8.3,8.3), color = color.black)
 box(pos = vec(0.5,31.7,0), opacity = 1, size = vec(2,34,5), color = vector(0.17,0.15,0.14))
+box(pos = vec(0.2, 0.8, 0), opacity = 1, size = vec(1,0.3,5), color = vector(0.9,0.77,0.58))
+box(pos = vec(0.3, 0.5, 0), opacity = 1, size = vec(1,2.1,9.5), color = vector(0.16,0.14,0.13))
+sphere(pos = vec(-0.3, 0.1, 2), opacity = 1, size = vec(0.5,0.5,0.5), color = vector(0.99,0.96,0.9))
+sphere(pos = vec(-0.3, 0.1, 0), opacity = 1, size = vec(0.5,0.5,0.5), color = vector(0.99,0.96,0.9))
+sphere(pos = vec(-0.3, 0.1, -2), opacity = 1, size = vec(0.5,0.5,0.5), color = vector(0.99,0.96,0.9))
+sphere(pos = vec(-0.3, 0.1, 1), opacity = 1, size = vec(0.5,0.5,0.5), color = vector(0.99,0.96,0.9))
+sphere(pos = vec(-0.3, 0.1, -1), opacity = 1, size = vec(0.5,0.5,0.5), color = vector(0.99,0.96,0.9))
+cylinder(pos = vec(-5,0,0), opacity = 1, size = vec(30,1,1), color = vector(0.82, 0.68, 3), axis = vec(0,30,0))
